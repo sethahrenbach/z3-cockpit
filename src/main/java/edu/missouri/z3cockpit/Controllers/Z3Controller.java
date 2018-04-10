ecbad85877d1e6c356a701a9cd4736eea6d552a2
SethK <seth.kurtenbach@gmail.com>
2018-04-10 17:49:19 -0500

    Change structure so that greeting.html is found by Spring

---
+++ src/main/java/edu/missouri/z3cockpit/Controllers/Z3Controller.java
@@ -0,0 +1,41 @@
+package edu.missouri.z3cockpit.Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+//@RequestMapping(value = "/z3")
+public class Z3Controller {
+
+    @GetMapping("/greeting")
+//    @ResponseBody
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        try {
+//            String pathToFile = "C:\\Users\\Seth\\Documents\\GitHub\\z3-cockpit\\out\\production\\resources\\static\\";
+//            String smtFile = args[0];
+//            String outFile = smtFile.split("\\.")[0];
+//            ProcessBuilder pb = new ProcessBuilder("z3", pathToFile + smtFile);
+//            final Process p = pb.start();
+//            BufferedReader br = new BufferedReader(
+//                    new InputStreamReader(p.getInputStream())
+//            );
+//            BufferedWriter bw = new BufferedWriter(
+//                    new FileWriter(new File("../" + outFile + ".out"))
+//            );
+//            String line;
+//            while((line = br.readLine())!=null) {
+//                bw.write(line);
+//            }
+//            bw.close();
+//        } catch (Exception ex) {
+//            System.out.println(ex);
+//        }
+//        System.out.println("Successful");
+//    }
+}
--- src/main/java/edu/missouri/z3cockpit/Z3CockpitApplication.java
+++ src/main/java/edu/missouri/z3cockpit/Z3CockpitApplication.java
@@ -2,11 +2,14 @@ package edu.missouri.z3cockpit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class Z3CockpitApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Z3CockpitApplication.class, args);
 	}
 }
